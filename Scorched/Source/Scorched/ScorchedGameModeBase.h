b6b80bf5b1ca9173ad373ec84d5cf7c1db5278a7
shane <sengelman@magicleap.com>
2020-01-17 12:43:27 -0500

    Initial commit

---
+++ Scorched/Source/Scorched/ScorchedGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ScorchedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCORCHED_API AScorchedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
